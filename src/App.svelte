2dd1be650deea881f3f8d18542d5efdb9dea8447
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-03-15 12:54:45 -0400

    add produce list for month

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,43 @@
 <script>
-	export let name;
+  import * as data from "../ontarioData.json";
+  import { getMonth } from "./helper";
+  const monthNumber = new Date().getMonth();
+  const month = getMonth(monthNumber);
+  const produce = data.default
+    .filter((item) => item.months.includes(monthNumber))
+    .map((item) => item.produce);
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>What's in your season?</h1>
+  <p>It's {month} nao</p>
+  <p>You can buy:</p>
+  <ul>
+    {#each produce as item}
+      <li>
+        {item}
+      </li>
+    {/each}
+  </ul>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
