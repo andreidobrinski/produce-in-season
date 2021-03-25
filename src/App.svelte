53164bd3eb1d2899bce59ba708a5ed3a13f360c3
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-03-25 17:46:00 -0400

    add colours

--- src/App.svelte
+++ src/App.svelte
@@ -29,14 +29,30 @@
   }
 
   h1 {
-    color: #ff3e00;
+    color: var(--purple);
     font-size: 4em;
     font-weight: 100;
+  }
+
+  ul {
+    padding: 0;
+  }
+
+  .month {
+    box-shadow: inset 0 -8px 0 0 var(--green);
   }
 
   @media (min-width: 640px) {
     main {
       max-width: none;
     }
+  }
+
+  :root {
+    --purple: rgb(61, 43, 86);
+    --blue: rgb(44, 73, 127);
+    --violet: rgb(128, 138, 159);
+    --green: rgb(186, 210, 159);
+    --lightGreen: rgb(204, 245, 172);
   }
 </style>
