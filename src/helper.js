dcbd83292aa751686bb2ae9fbd779af2410b905d
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-04-01 17:17:23 -0400

    add emoji map and functions

--- src/helper.js
+++ src/helper.js
@@ -16,3 +16,89 @@ const monthMap = {
 export function getMonth(number) {
   return monthMap[number];
 };
+
+const produceEmojiMap = {
+  Apples: ['🍎','🍏'],
+  Apricots: '🍊',
+  Blueberries: '🫐',
+  Cherries: '🍒',
+  Crabapples: null,
+  Cranberries: null,
+  Currants: null,
+  Gooseberries: null,
+  Grapes: '🍇',
+  Muskmelon: '🍈',
+  Nectarines: '🍊',
+  Peaches: '🍑',
+  Pears: '🍐',
+  Plums: null,
+  Raspberries: null,
+  Rhubarb: null,
+  "Strawberries (Field)": '🍓',
+  "Strawberries (Greenhouse)": '🍓',
+  Watermelon: '🍉',
+  Artichoke: null,
+  "Asian Vegetables": ['🥦', '🥬'],
+  Asparagus: null,
+  "Beans (Green/Yellow)": null,
+  Beets: null,
+  "Bitter Melon/Fuzzy Squash": null,
+  "Bok Choy": null,
+  Broccoli: '🥦',
+  "Brussels Sprouts": null,
+  Cabbage: null,
+  Carrots: '🥕',
+  Cauliflower: null,
+  Celery: null,
+  "Chinese Broccoli (White Flower)": '🥦',
+  Corn: '🌽',
+  "Cucumber (Field)": '🥒',
+  "Cucumber (Greenhouse)": '🥒',
+  "Daikon Radish": null,
+  "Edible Amaranth (Hinn Choy)": null,
+  Eggplant: '🍆',
+  Garlic: '🧄',
+  Leeks: null,
+  "Lettuce (Assorted)": '🥬',
+  "Lettuce (Greenhouse)": '🥬',
+  Mushrooms: '🍄',
+  "Mustard Greens (Gai Choy)": '🌿',
+  "Napa Cabbage": '🥬',
+  "Onions (Cooking)": '🧅',
+  "Onions (Green)": '🧅',
+  "Onions (Red)": '🧅',
+  Parsnips: null,
+  "Peas (Green)": null,
+  "Peas (Snow)": null,
+  "Peppers (Field)": ['🫑', '🌶️'],
+  "Peppers (Greenhouse)": ['🫑', '🌶️'],
+  Potatoes: '🥔',
+  Pumpkin: '🎃',
+  Radicchio: null,
+  Radishes: null,
+  Rapini: '🥬',
+  Rutabaga: null,
+  "Snow Pea Shoots (Dow Miu)": null,
+  Spinach: '🥬',
+  Sprouts: null,
+  Squash: null,
+  "Summer squash": null,
+  "Sweet potatoes": '🥔',
+  "Tomatoes (Field)": '🍅',
+  "Tomatoes (Greenhouse)": '🍅',
+  "Water Spinach (Tung Choy)": null,
+  "Yow Choy (Yellow Flower)": null,
+  Zucchini: null,
+};
+
+export const getProduceEmojis = (produce) => {
+  return produce
+    .flatMap(item => produceEmojiMap[item.produce])
+    .filter(item => !!item)
+};
+
+export const getRandomEmoji = (produce) => {
+  const emojis = getProduceEmojis(produce);
+  const randomIndex = Math.floor(Math.random() * emojis.length);
+  return emojis[randomIndex];
+}
